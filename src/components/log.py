b1a5c56b936c6e63ec0cbcff1743c51b6ca95e78
Nazib Dimal <dioexsys@gmail.com>
2024-10-28 21:12:40 +0800

    add log component

---
+++ src/components/log.py
@@ -0,0 +1,14 @@
+import customtkinter as ctk
+
+from modules.log_client import LogClient
+class Log(ctk.CTkTextbox):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, font=("Calibri", 18), **kwargs)
+        self.grid(row=1, column=0, sticky="nsew", padx=25, pady=(0,25))
+        
+        self.logger = LogClient()
+        self.logger.current_log.trace_add('write', self.add_log)
+        
+    def add_log(self, *args):
+        self.insert("end", self.logger.current_log.get() + "\n")
+        self.yview("end")
